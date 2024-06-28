163cec0f7a479f128d919383b33dfa5f27f4d812
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-06-27 20:56:05 -0400

    Program working with multiple files
    The Program can operate with all the parsing related function seperated
    into Parse.cpp. The new main file in DannyBasic.cpp.

--- CVersion/DannyBasic.cpp
+++ CVersion/DannyBasic.cpp
@@ -1,4 +1,44 @@
+#include <iostream>
+#include <fstream>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <list>
+// #include <cstring>
+// #include <string>
+// #include <vector>
+
+#include "Parse.h"
+// #include "Syntax.h"
+// #include "Token.h"
+
+using namespace std;
 int main(int argc, char *argv[]) {
 
-  return 0;
+  // Creating and Opening File Object
+  ifstream source("test.bsc");
+
+  // File Does Not Exist
+  if(!source.good()){
+    cout << "Program Does Not Exist or Missing File"<< endl;
+  }
+
+  // Print the Original Source Code
+  printSourceCode(&source);
+  source.close();
+
+  // Reset file pointer and start parsing
+  source.open("test.bsc");
+
+  // Parses Source File
+
+
+  // What I want is A list of Tokens Created by the Parsed File
+  TokenList* tl = startParse(&source);
+
+  // // DEBUG: Prints All Tokens
+  tl->printAllTokens();
+
+  // // Closes File and Exits Program
+  // source.close();
+  // return 0;
 }
---
+++ CVersion/Parse.h
@@ -0,0 +1,78 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
+#include <cstring>
+#include <string>
+#include <vector>
+using namespace std;
+// This is the Parser for DannyBasic: A compiler for TinyBasic made by Daniel Small
+
+enum keyword {
+  ID,
+  NUMBER,
+  CR,
+  CLEAR,
+  END,
+  GOTO,
+  GOSUB,
+  INPUT,
+  IF,
+  LET,
+  LIST,
+  PRINT,
+  RETURN,
+  RUN,
+  THEN,
+  LSS,
+  LEQ,
+  NEQ,
+  NNEQ,
+  GEQ,
+  GTR,
+  ADD,
+  SUB,
+  DIV,
+  MOD,
+  MULT,
+  STRING,
+  EQL,
+  EQUALITY
+};
+class Token {
+public:
+  Token(keyword tT, int number, string name, string contents);
+};
+class TokenList{
+public:
+  void addToken(keyword type, int number, string name, string contents);
+  void printAllTokens();
+};
+
+//Print the Source Code
+void printSourceCode(std::ifstream* source);
+
+// Print Error and Exits Program
+void error(int num);
+
+// Automata that Detects Tokens
+int newline(ifstream* source);
+int clear(ifstream* source);
+int end(ifstream* source);
+int chooseG(ifstream* sourece);
+int chooseI(ifstream* source);
+int chooseL(ifstream* source);
+int print(ifstream* source);
+int chooseR(ifstream* source);
+int then(ifstream* source);
+int String(ifstream* source);
+int id(ifstream* source);
+int number(ifstream* source);
+int symbol(ifstream* source);
+TokenList* startParse(ifstream* source);
+
+#endif
