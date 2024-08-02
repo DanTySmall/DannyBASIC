918b5f241adf5b9e710e07e4db7d04298b555c7b
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-08-02 00:21:47 -0400

    Executable can be parsed

--- CVersion/Execute.cpp
+++ CVersion/Execute.cpp
@@ -9,10 +9,54 @@ int execute(){
   cout << "Im Executing" << endl;
 
   char c;
+  int n;
+
+  vector<int> memory;
+  int instruction;
+
   // Parsing Instructions
-  while (input.peek() != '\0'){
+  while (input >> instruction){
+    memory.push_back(instruction);
+    input >> c;
+    if(c == '\0') {
+      cout<< "break" << endl;
+      break;
+    }else{
+      input.putback(c);
+    }
+    cout << instruction<< endl;
+  }
+
+  vector<char> strings;
+
+  // Parsing Strings
+  while (input.get(c)){
     input.get(c);
     cout << c;
+    if(c == '\0') {
+      input >> c;
+      cout<< c;
+      if(c == '\0'){
+        break;
+      }else{
+        input.putback(c);
+      }
+
+    }else{
+      input.putback(c);
+    }
+
   }
+
+  // Parsing Line Address
+  cout << endl;
+  while (input >> n) {
+    cout << n << endl;
+    input.get(c);
+  }
+
+
+  cout <<  (input.good()) << endl;
+
   return 1;
 }
